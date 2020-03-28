a17d969cf2c4ead2d22910bfc8c672a51e683a40
Ashley Winters <ashbwinters@gmail.com>
2020-03-28 20:32:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,78 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  # outputs welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  # generates and returns a random number between 1 and 11.
+  new_card = rand(1..11)
+  new_card
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  # give the player their current card total
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  # asks the player to hit(h) or stay(s)
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  # stores player input
+  user_input = gets.chomp
+  user_input
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  # bust! game over!
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  # deals two cards and gives total
+  card_one = deal_card 
+  card_two = deal_card
+  total = card_one + card_two
+  display_card_total(total)
+  total
 end
 
-def hit?
-  # code hit? here
-end
+def hit?(current_card_total)
+  # ask for input,if/else argument to deal new card or not
+  if current_card_total > 21
+    end_game(current_card_total)
+  else
+    prompt_user
+    
+    user_input = get_user_input
+    
+    if user_input == "s" or "h"
+      if user_input == "s"
+        current_card_total
+      end
+      if user_input == "h"
+        new_card = deal_card
+        current_card_total += new_card
+          if current_card_total <= 21
+          display_card_total(current_card_total)
+          end
+          current_card_total
+      end
+    end
+    if user_input != "s" or "h" 
+      invalid_command
+      current_card_total
+    end
+  end
+ end
 
 def invalid_command
-  # code invalid_command here
+  # if player input is not hit or stay, asks again.
+  puts "Please enter a valid command"
 end
 
 #####################################################
